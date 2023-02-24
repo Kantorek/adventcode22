fa258d4436773b33a624147ad1dddd9f1bef6e9f
Kantorek <jakubklimek9@wp.pl>
2023-02-24 03:11:26 +0100

    works

--- day3/main.py
+++ day3/main.py
@@ -1,7 +1,18 @@
 low_crt = 96 # sub to get correct val {1, 26}
 up_crt = 38 # sub to get correct val {27, 52}
+sum = 0
 for line in open('day3/input'):
   line_center = int((len(line) - 1) / 2)
-  print(line_center)
-  print(line[:11])
-  print(line[11:])
+  for letter in line[:line_center]:
+    if letter in line[line_center:]:
+      print(letter)
+      if letter.isupper():
+        sum = sum + ord(letter) - up_crt
+      elif letter.islower():
+        sum = sum + ord(letter) - low_crt
+      else:
+        print("???")
+        break
+      break
+
+print("Sum of all repeting letters is: {}".format(sum))
