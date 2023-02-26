87f7a8558aaca0a36b2080d6ae1f9ccc50d65518
Kantorek <jakubklimek9@wp.pl>
2023-02-26 13:01:27 +0100

    remove don't work

--- day4/main.py
+++ day4/main.py
@@ -1,14 +1,14 @@
 first_sectors = []
 second_sectors = []
 sum_of_overlaps = 0
-for line in open('day4/input'):
+for line in open('day4/test'):
   line = line.replace('\n', '')
   line = line.replace('-', ',')
   line = line.split(',')
   print(line)
   for id in range(int(line[0]), int(line[1])+1):
     first_sectors.append(id)
-  for id in range(int(line[0]), int(line[1])+1):
+  for id in range(int(line[2]), int(line[3])+1):
     second_sectors.append(id)
   print(first_sectors, second_sectors)
   if len(first_sectors) > len(second_sectors):
@@ -16,14 +16,15 @@ for line in open('day4/input'):
       if sec not in first_sectors:
         continue
       second_sectors.remove(sec)
-    if second_sectors == None:
-      sum = sum + 1
+    if not second_sectors:
+      sum_of_overlaps = sum_of_overlaps + 1
   elif len(second_sectors) >= len(first_sectors):
     for sec in second_sectors:
       if sec not in second_sectors:
         continue
       first_sectors.remove(sec)
-
-    if first_sectors == None:
-      sum = sum + 1
-print(sum)
+    if not first_sectors:
+      sum_of_overlaps = sum_of_overlaps + 1
+    first_sectors.clear()
+    second_sectors.clear()
+print(sum_of_overlaps)
