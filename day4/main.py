e87aa0deafdb7aac9265f6d5b06c2bc49386e457
Kantorek <jakubklimek9@wp.pl>
2023-02-26 03:23:21 +0100

    inf loop?

--- day4/main.py
+++ day4/main.py
@@ -13,9 +13,9 @@ for line in open('day4/input'):
   print(first_sectors, second_sectors)
   if len(first_sectors) > len(second_sectors):
     for sec in first_sectors:
-      second_sectors.remove(sec)
       if sec not in first_sectors:
         continue
+      second_sectors.remove(sec)
     if second_sectors == None:
       sum = sum + 1
   elif len(second_sectors) >= len(first_sectors):
@@ -23,6 +23,7 @@ for line in open('day4/input'):
       if sec not in second_sectors:
         continue
       first_sectors.remove(sec)
+
     if first_sectors == None:
       sum = sum + 1
 print(sum)
