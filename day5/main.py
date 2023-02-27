4294a9e03ba0898f520a9fe1d38b62be33fa3344
Kantorek <jakubklimek9@wp.pl>
2023-02-28 00:21:34 +0100

    stack table

--- day5/main.py
+++ day5/main.py
@@ -1,3 +1,8 @@
 for line in open('day5/input.txt'):
-
+  
+  if line == 'move 2 from 5 to 9\n':
+    break
+  lineEmp = line.replace('    ', ' [ ]')
+  line = lineEmp.replace('[', '')
+  line = line.split(']')
   print(line)
