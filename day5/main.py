d1f6d99a5f111cde835cb33f035f73c09b992111
Kantorek <jakubklimek9@wp.pl>
2023-03-01 00:06:52 +0100

    better col

--- day5/main.py
+++ day5/main.py
@@ -1,9 +1,9 @@
 for line in open('day5/input.txt'):
   
-  if line == 'move 2 from 5 to 9\n':
+  if line == '\n':
     break
   lineEmp = line.replace('    ', ' [0]')
   line = lineEmp.replace('[', '')
-  line = line.replace('\n', ' ')
+  line = line.replace(']\n', '')
   line = line.split('] ')
   print(line)
