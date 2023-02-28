dc52aa713c2b43e9bc195ea4c579a74b401d934b
Kantorek <jakubklimek9@wp.pl>
2023-02-28 23:56:26 +0100

    del \n

--- day5/main.py
+++ day5/main.py
@@ -2,7 +2,8 @@ for line in open('day5/input.txt'):
   
   if line == 'move 2 from 5 to 9\n':
     break
-  lineEmp = line.replace('    ', ' [ ]')
+  lineEmp = line.replace('    ', ' [0]')
   line = lineEmp.replace('[', '')
-  line = line.split(']')
+  line = line.replace('\n', ' ')
+  line = line.split('] ')
   print(line)
