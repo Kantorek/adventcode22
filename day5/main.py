d0b288eac1b6cec8b6289f006af8b33d9181c97e
Kantorek <jakubklimek9@wp.pl>
2023-03-01 18:45:07 +0100

    only cont

--- day5/main.py
+++ day5/main.py
@@ -1,9 +1,10 @@
 for line in open('day5/input.txt'):
   
-  if line == '\n':
+  if line[0] != '[':
     break
   lineEmp = line.replace('    ', ' [0]')
   line = lineEmp.replace('[', '')
   line = line.replace(']\n', '')
+  # line = line.replace('\n', '')
   line = line.split('] ')
   print(line)
