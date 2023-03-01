3c19ef9848732d19310123eceb482122f7d8447e
Kantorek <jakubklimek9@wp.pl>
2023-03-01 19:15:13 +0100

    col array

--- day5/main.py
+++ day5/main.py
@@ -1,3 +1,4 @@
+crates = [ [] for _ in range(9)]
 for line in open('day5/input.txt'):
   
   if line[0] != '[':
@@ -5,6 +6,9 @@ for line in open('day5/input.txt'):
   lineEmp = line.replace('    ', ' [0]')
   line = lineEmp.replace('[', '')
   line = line.replace(']\n', '')
-  # line = line.replace('\n', '')
   line = line.split('] ')
+  for col in range(len(line)):
+    if line[col] != '0':
+      crates[col].append(line[col])
   print(line)
+print(crates)
