eff0abfbb8a0b222ac1ad7f48bc83db948bf20d2
Kantorek <jakubklimek9@wp.pl>
2023-03-12 00:07:44 +0100

    dir_size

--- day7/main.py
+++ day7/main.py
@@ -1,5 +1,11 @@
+dir_list = {}
+def is_dir(x: str):
+  if x[0:3] == 'dir' and x[4] not in dir_list.keys():
+    dir_list.update({x[4]: 0})
 term_data = []
 for data in open('day7/test'):
   data = data.replace('\n', '')
   term_data.append(data)
+  is_dir(data)
 print(term_data)
+print(dir_list)
