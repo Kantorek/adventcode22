77c710ac79667684618fc248f7fd7364966e8b43
Kantorek <jakubklimek9@wp.pl>
2023-03-18 16:33:02 +0100

    sum of files

--- day7/main.py
+++ day7/main.py
@@ -1,6 +1,7 @@
-dir_list = {'home/': 0}
+dir_list = {'home': 0}
 path = 'home'
-for com in open('day7/test'):
+is_listing = False
+for com in open('day7/input'):
   com = com.replace('\n', '')
   if com[2:] == 'cd /':
     pass
@@ -9,4 +10,13 @@ for com in open('day7/test'):
   elif com[2:4] == 'cd':
     path = path + '/' + com[5:]
     dir_list.update({path: 0})
+  elif com[2:4] == 'ls':
+    is_listing = True
+  elif is_listing:
+    if com[0] == '$':
+      is_listing = False
+      continue
+    if com[0] != 'd':
+      com = com[:com.rfind(' ')]
+      dir_list[path] = dir_list[path] + int(com)
 print(dir_list)
