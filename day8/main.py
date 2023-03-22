f47e3d74ceeede3aa223db9eb4e70bb93fc764a5
Kantorek <jakubklimek9@wp.pl>
2023-03-22 23:32:56 +0100

    edge trees

--- day8/main.py
+++ day8/main.py
@@ -1,5 +1,10 @@
 forrest = []
+tree_see = 0
 for line in open('day8/test'):
   line = line.replace('\n', '')
   forrest.append(line)
+width = len(forrest)
+length = len(forrest[0])
 print(forrest)
+tree_see = 2 * (width + length) - 4
+print(tree_see)
