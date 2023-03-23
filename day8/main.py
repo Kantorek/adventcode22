4fdd6daf38cdcf30b56757abe87e8212d7d1320e
Kantorek <jakubklimek9@wp.pl>
2023-03-23 23:58:42 +0100

    passing by all trees

--- day8/main.py
+++ day8/main.py
@@ -7,4 +7,7 @@ width = len(forrest)
 length = len(forrest[0])
 print(forrest)
 tree_see = 2 * (width + length) - 4
-print(tree_see)
+print("Trees on the edge: {}".format(tree_see))
+for i, u2d in enumerate(forrest[1:-1]):
+  for j, l2r in enumerate(u2d[1:-1]):
+    pass
