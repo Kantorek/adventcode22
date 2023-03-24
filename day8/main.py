ff2b0300ff2a33c059ba45366af949f46835e37d
Kantorek <jakubklimek9@wp.pl>
2023-03-24 18:06:58 +0100

    TODO: change list of str to int

--- day8/main.py
+++ day8/main.py
@@ -10,4 +10,9 @@ tree_see = 2 * (width + length) - 4
 print("Trees on the edge: {}".format(tree_see))
 for i, u2d in enumerate(forrest[1:-1]):
   for j, l2r in enumerate(u2d[1:-1]):
-    pass
+    # TODO: List is string not int
+    if max(forrest[0:j+1]) < l2r:
+      tree_see = tree_see + 1
+    elif max(forrest[j+1:]) < l2r:
+      tree_see = tree_see + 1
+    
