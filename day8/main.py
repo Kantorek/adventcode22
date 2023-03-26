f4861c5baf50191be814fc1f99de30eb6513ebe0
Kantorek <jakubklimek9@wp.pl>
2023-03-26 20:20:41 +0200

    test pass

--- day8/main.py
+++ day8/main.py
@@ -2,7 +2,7 @@ forrest = []
 tree_see = 0
 for line in open('day8/test'):
   line = line.replace('\n', '')
-  forrest.append(line)
+  forrest.append(list(map(int,line)))
 width = len(forrest)
 length = len(forrest[0])
 print(forrest)
@@ -10,9 +10,12 @@ tree_see = 2 * (width + length) - 4
 print("Trees on the edge: {}".format(tree_see))
 for i, u2d in enumerate(forrest[1:-1]):
   for j, l2r in enumerate(u2d[1:-1]):
-    # TODO: List is string not int xd
-    if max(forrest[0:j+1]) < l2r:
+    if max(forrest[i+1][:j+1]) < l2r:
+      print('TREE!')
+      print(l2r)
       tree_see = tree_see + 1
-    elif max(forrest[j+1:]) < l2r:
+    elif max(forrest[i+1][j+2:]) < l2r:
+      print('TREE!')
+      print(l2r)
       tree_see = tree_see + 1
-    
+print("Trees on the edge: {}".format(tree_see))
