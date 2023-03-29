84a3b6799b56e5962a11eeba15f7de57f6a87e1b
Kantorek <jakubklimek9@wp.pl>
2023-03-30 01:03:16 +0200

    done

--- day8/main.py
+++ day8/main.py
@@ -9,20 +9,21 @@ length = len(forrest[0])
 tree_see = 2 * (width + length) - 4
 print("Trees on the edge: {}".format(tree_see))
 for i, u2d in enumerate(forrest[1:-1]):
-  for col in range(width):
-    col_for.append(forrest[col][i+1])
   for j, l2r in enumerate(u2d[1:-1]):
+    for col in range(width):
+      col_for.append(forrest[col][j+1])
     if max(forrest[i+1][:j+1]) < l2r:
       print(forrest[i+1][:j+1])
       tree_see = tree_see + 1
     elif max(forrest[i+1][j+2:]) < l2r:
       print(forrest[i+1][j+2:])
       tree_see = tree_see + 1
-    elif max(col_for[:j+1]) < l2r:
-      print(col_for[:j+1])
+    elif max(col_for[:i+1]) < l2r:
+      print(col_for[:i+1])
       tree_see = tree_see + 1
-    elif max(col_for[j+2:]) < l2r:
-      print(col_for[j+2:])
+    elif max(col_for[i+2:]) < l2r:
+      print(col_for[i+2:])
       tree_see = tree_see + 1
-  col_for = []
-print("Trees on the edge: {}".format(tree_see))
+    col_for = []
+print("Visible trees: {}".format(tree_see))
+# 1827
