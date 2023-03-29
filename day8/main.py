1d39c6c1ee3e1a660874bf57e99dcf368886bec7
Kantorek <jakubklimek9@wp.pl>
2023-03-29 20:07:55 +0200

    wrong ans

--- day8/main.py
+++ day8/main.py
@@ -1,21 +1,28 @@
 forrest = []
 tree_see = 0
-for line in open('day8/test'):
+col_for = []
+for line in open('day8/input'):
   line = line.replace('\n', '')
   forrest.append(list(map(int,line)))
 width = len(forrest)
 length = len(forrest[0])
-print(forrest)
 tree_see = 2 * (width + length) - 4
 print("Trees on the edge: {}".format(tree_see))
 for i, u2d in enumerate(forrest[1:-1]):
+  for col in range(width):
+    col_for.append(forrest[col][i+1])
   for j, l2r in enumerate(u2d[1:-1]):
     if max(forrest[i+1][:j+1]) < l2r:
-      print('TREE!')
-      print(l2r)
+      print(forrest[i+1][:j+1])
       tree_see = tree_see + 1
     elif max(forrest[i+1][j+2:]) < l2r:
-      print('TREE!')
-      print(l2r)
+      print(forrest[i+1][j+2:])
       tree_see = tree_see + 1
+    elif max(col_for[:j+1]) < l2r:
+      print(col_for[:j+1])
+      tree_see = tree_see + 1
+    elif max(col_for[j+2:]) < l2r:
+      print(col_for[j+2:])
+      tree_see = tree_see + 1
+  col_for = []
 print("Trees on the edge: {}".format(tree_see))
