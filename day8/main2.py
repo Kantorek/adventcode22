eee9ea4e5c9a3658042cdba95bfc616b46b7270f
Kantorek <jakubklimek9@wp.pl>
2023-04-06 13:34:21 +0200

    cross trees

--- day8/main2.py
+++ day8/main2.py
@@ -1,14 +1,19 @@
 forrest = []
 tree_point = 0
 col_for = []
-for line in open('day8/input'):
+for line in open('day8/test'):
   line = line.replace('\n', '')
   forrest.append(list(map(int,line)))
-width = len(forrest)
-length = len(forrest[0])
+
 print("Trees on the edge: {}".format(tree_point))
-for i, u2d in enumerate(forrest[1:-1]):
-  for j, l2r in enumerate(u2d[1:-1]):
-    
+for i, u2d in enumerate(forrest):
+  for j, l2r in enumerate(u2d):
+    for col in range(len(forrest[0])):
+      col_for.append(forrest[col][j])
+    north = col_for[:i]
+    south = col_for[i+1:]
+    west = u2d[:j]
+    east = u2d[j+1:]
+    print(north, south, west, east)
     col_for = []
 print("Visible trees: {}".format(tree_point))
