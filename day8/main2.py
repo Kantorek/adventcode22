a7a0cd856389fd77037e6494ba598a2bdcbf5cd8
Kantorek <jakubklimek9@wp.pl>
2023-04-06 14:23:37 +0200

    tree_score fun

--- day8/main2.py
+++ day8/main2.py
@@ -1,19 +1,29 @@
+def tree_score(trees: list) -> int:
+  tree_points = 1
+  highest_tree = trees.pop()
+  while trees:  
+    if trees[-1] >= highest_tree:
+      tree_points = tree_points + 1
+      highest_tree = trees[-1]
+    trees.pop()
+  return tree_points
 forrest = []
-tree_point = 0
+tree_score = 0
 col_for = []
 for line in open('day8/test'):
   line = line.replace('\n', '')
   forrest.append(list(map(int,line)))
 
-print("Trees on the edge: {}".format(tree_point))
 for i, u2d in enumerate(forrest):
   for j, l2r in enumerate(u2d):
     for col in range(len(forrest[0])):
       col_for.append(forrest[col][j])
+    c_tree = u2d
     north = col_for[:i]
-    south = col_for[i+1:]
+    south = col_for[i+1:].reverse()
     west = u2d[:j]
-    east = u2d[j+1:]
-    print(north, south, west, east)
+    east = u2d[j+1:].reverse()
+    
+      
     col_for = []
 print("Visible trees: {}".format(tree_point))
