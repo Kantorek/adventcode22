7448ad30b25e876099cd7d69dc759da3a478fc6e
Kantorek <jakubklimek9@wp.pl>
2023-04-06 21:08:50 +0200

    tbd

--- day8/main2.py
+++ day8/main2.py
@@ -1,19 +1,21 @@
-def tree_view(trees: list) -> int:
+def tree_view(trees: list, x_tree: int) -> int:
   tree_points = 0
   if trees:
     highest_tree = trees.pop()
     tree_points = 1
   while trees:  
-    if trees[-1] > highest_tree:
-      tree_points = tree_points + 1
+    if trees[-1] >= highest_tree:
+      tree_points += 1
       highest_tree = trees[-1]
+    if highest_tree >= x_tree:
+      return tree_points
     trees.pop()
   return tree_points
 
 forrest = []
 tree_score = 0
 col_for = []
-for line in open('day8/test'):
+for line in open('day8/input'):
   line = line.replace('\n', '')
   forrest.append(list(map(int,line)))
 
@@ -32,9 +34,8 @@ for i, u2d in enumerate(forrest):
     east_r = list()
     for item in east:
       east_r = [item] + east_r
-    x = tree_view(north)*tree_view(south_r)*tree_view(west)*tree_view(east_r)
-    print(x, l2r)
-    tree_score = max(tree_score, x)
+    x = tree_view(north, l2r)*tree_view(south_r, l2r)*tree_view(west, l2r)*tree_view(east_r, l2r)
+    tree_score += x
     col_for = []
-    print(tree_score)
-print("Visible trees: {}".format(tree_score))
+print("The highest score: {}".format(tree_score))
+# 335580
