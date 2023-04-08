256b17619d10ececfd62a4f3ee5e8219af5161fd
Kantorek <jakubklimek9@wp.pl>
2023-04-08 23:03:39 +0200

    central_point

--- day9/main.py
+++ day9/main.py
@@ -1,6 +1,9 @@
-matrix = [[0 for x in range(100)] for _ in range(100)]
+size = 20
+matrix = [['-' for x in range(size)] for _ in range(size)]
+x, y = int(len(matrix[0])/2), int(len(matrix[0])/2)
 for line in open('day9/test'):
   way = line[0]
   length = int(line[2])
+matrix[x][y] = 's'
 for row in matrix:
   print(row)
