1fb2353834d84c5d15448f2483f8572ee5cc40d5
Kantorek <jakubklimek9@wp.pl>
2023-04-09 15:46:04 +0200

    marking_head

--- day9/main.py
+++ day9/main.py
@@ -1,9 +1,33 @@
-size = 20
+size = 6
 matrix = [['-' for x in range(size)] for _ in range(size)]
+step = 0
 x, y = int(len(matrix[0])/2), int(len(matrix[0])/2)
+x, y = 5, 0
+matrix[x][y] = 's'
+
 for line in open('day9/test'):
   way = line[0]
   length = int(line[2])
-matrix[x][y] = 's'
+  if way == 'D':
+    while length:
+      x += 1
+      matrix[x][y] = '#'
+      length -= 1
+  elif way == 'U':
+    while length:
+      x -= 1
+      matrix[x][y] = '#'
+      length -= 1
+  elif way == 'L':
+    while length:
+      y -= 1
+      matrix[x][y] = '#'
+      length -= 1
+  elif way == 'R':
+    while length:
+      y += 1
+      matrix[x][y] = '#'
+      length -= 1
+
 for row in matrix:
   print(row)
