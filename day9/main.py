97dce57dd8cacc03d5a5532796fb8b27204b3f36
Kantorek <jakubklimek9@wp.pl>
2023-04-09 20:33:02 +0200

    last_head_pos

--- day9/main.py
+++ day9/main.py
@@ -1,11 +1,15 @@
-def tail(head_p: tuple(int, int), tail_p: tuple(int, int)):
-  pass
+def tail(x_head: int, y_head: int, x_tail: int, y_tail: int) -> (int, int):
+  if abs(x_head-x_tail) > 1 or abs(y_head-y_tail) > 1:
+    matrix[y_tail][x_tail] = '#'
+    x_tail = x_head_p
+    y_tail = y_head_p
 
 size = 6
 matrix = [['-' for x in range(size)] for _ in range(size)]
 step = 0
 x, y = int(len(matrix[0])/2), int(len(matrix[0])/2)
 x, y = 0, 5
+x_t, y_t = 0, 5
 matrix[x][y] = 's'
 
 for line in open('day9/test'):
@@ -13,23 +17,27 @@ for line in open('day9/test'):
   length = int(line[2])
   if way == 'D':
     while length:
+      y_p = y 
       y += 1
-      matrix[x][y] = '#'
+      tail(x, y, x_t, y_t)
       length -= 1
   elif way == 'U':
     while length:
+      y_p = y
       y -= 1
-      matrix[x][y] = '#'
+      tail(x, y, x_t, y_t)
       length -= 1
   elif way == 'L':
     while length:
+      x_p = x
       x -= 1
-      matrix[x][y] = '#'
+      tail(x, y, x_t, y_t)
       length -= 1
   elif way == 'R':
     while length:
+      x_p = x
       x += 1
-      matrix[x][y] = '#'
+      tail(x, y, x_t, y_t)
       length -= 1
 
 for row in matrix:
