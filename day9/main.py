561e1fd5f703bb04cc1985d07808996739f6b3aa
Kantorek <jakubklimek9@wp.pl>
2023-04-09 21:00:54 +0200

    uncorrect_tail_mark

--- day9/main.py
+++ day9/main.py
@@ -1,16 +1,20 @@
-def tail(x_head: int, y_head: int, x_tail: int, y_tail: int) -> (int, int):
-  if abs(x_head-x_tail) > 1 or abs(y_head-y_tail) > 1:
+def tail(x_head: int, y_head: int, x_tail: int, y_tail: 
+  int, x_prev: int, y_prev: int) -> list:
+  if abs(x_head-x_tail) >= 1 or abs(y_head-y_tail) >= 1:
     matrix[y_tail][x_tail] = '#'
-    x_tail = x_head_p
-    y_tail = y_head_p
+    return [x_prev, y_prev]
+  return [x_tail, y_tail]
+
 
 size = 6
 matrix = [['-' for x in range(size)] for _ in range(size)]
 step = 0
-x, y = int(len(matrix[0])/2), int(len(matrix[0])/2)
-x, y = 0, 5
-x_t, y_t = 0, 5
-matrix[x][y] = 's'
+# y, x = int(len(matrix[0])/2), int(len(matrix[0])/2)
+y, x = 5, 0
+x_p, y_p = 0, 0
+y_t, x_t = 5, 0
+new_tail = [0, 1]
+matrix[y][x] = 's'
 
 for line in open('day9/test'):
   way = line[0]
@@ -19,25 +23,33 @@ for line in open('day9/test'):
     while length:
       y_p = y 
       y += 1
-      tail(x, y, x_t, y_t)
+      new_tail = tail(x, y, x_t, y_t, x_p, y_p)
+      x_t = new_tail[0]
+      y_t = new_tail[1]
       length -= 1
   elif way == 'U':
     while length:
       y_p = y
       y -= 1
-      tail(x, y, x_t, y_t)
+      new_tail = tail(x, y, x_t, y_t, x_p, y_p)
+      x_t = new_tail[0]
+      y_t = new_tail[1]
       length -= 1
   elif way == 'L':
     while length:
       x_p = x
       x -= 1
-      tail(x, y, x_t, y_t)
+      new_tail = tail(x, y, x_t, y_t, x_p, y_p)
+      x_t = new_tail[0]
+      y_t = new_tail[1]
       length -= 1
   elif way == 'R':
     while length:
       x_p = x
       x += 1
-      tail(x, y, x_t, y_t)
+      ew_tail = tail(x, y, x_t, y_t, x_p, y_p)
+      x_t = new_tail[0]
+      y_t = new_tail[1]
       length -= 1
 
 for row in matrix:
