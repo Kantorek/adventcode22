dc1cb9d76a067a9ca13a45528ddef17aeafe265f
Kantorek <jakubklimek9@wp.pl>
2023-04-22 22:43:25 +0200

    one_star DONE

--- day10/main.py
+++ day10/main.py
@@ -1,9 +1,25 @@
 cycle = 0
-for line in open('day10/test'):
+check = [19, 59, 99, 139, 179, 219]
+sum_check = []
+x = 1
+
+for line in open('day10/input'):
   if line[0] == 'n':
     cycle += 1
-    continue
-  com, val  = line.split()
-  val = int(val)
-  print(val)
-print(cycle)
+    if cycle in check:
+      print(x, (cycle+1) * x)
+      sum_check.append((cycle+1) * x)
+  else: 
+    com, val  = line.split()
+    val = int(val)
+    cycle += 1
+    if cycle in check:
+      print(x, (cycle+1) * x)
+      sum_check.append((cycle+1) * x)
+    cycle += 1
+    x += val
+    if cycle in check:
+      print(x, (cycle+1) * x)
+      sum_check.append((cycle+1) * x)
+    
+print(sum(sum_check))
