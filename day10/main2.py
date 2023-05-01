decdd7c91c0bc45a9876a64cad3f699d6b2dd618
Kantorek <jakubklimek9@wp.pl>
2023-05-01 21:34:31 +0200

    cnt

--- day10/main2.py
+++ day10/main2.py
@@ -2,39 +2,29 @@ cycle = 0
 # check = [39, 79, 119, 159, 199]
 check = [40, 80, 120, 160, 200]
 line = []
-x = 0
+x = 1
+
+def sprite(c: int, pos: int, line: list) -> str:
+  if c in check:
+    print()
+    line = []
+    return line
+  else:
+    if pos + 1 > c and pos - 1 < c:
+      return '#'
+    else:
+      return '.'
 
 for l in open('day10/test'):
-  if l[0] == 'n':
+  if l == 'noop\n':
     cycle += 1
-    if cycle in check:
-      print(''.join(line))
-      line = []
-      cycle = 0
-    if abs(cycle-x) <= 1:
-      line += '#'
-    else:
-      line += '.'
-    
+    line.append(sprite(cycle, x, line))
   else: 
     com, val  = l.split()
     val = int(val)
     cycle += 1
-    if cycle in check:
-      print(''.join(line))
-      line = []
-      cycle = 0
-    if abs(cycle-x) <= 1:
-      line += '#'
-    else:
-      line += '.'
+    line.append(sprite(cycle, x, line))
     cycle += 1
-    if cycle in check:
-      print(''.join(line))
-      line = []
-      cycle = 0
-    if abs(cycle-x) <= 1:
-      line += '#'
-    else:
-      line += '.'
     x += val
+    line.append(sprite(cycle, x, line))
+
